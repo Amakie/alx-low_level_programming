3a3007b2a7a7f0540c3b6524ffb35ea9a9357dcd
Constance Amatey <consamate57@gmail.com>
2023-02-17 13:46:27 -0800

    c1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long long int e;
 char d;
 float b;
 
-printf("Size of a char: %ld bytes\n", (unsigned long)sizeof(d));
-printf("Size of an int: %ld bytes\n", (unsigned long)sizeof(a));
-printf("Size of a long int: %ld bytes\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %ld bytes\n", (unsigned long)sizeof(e));
-printf("Size of a float: %1d bytes\n", (unsigned long)sizeof(b));
+printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(e));
+printf("Size of a float: %1d byte(s)\n", (unsigned long)sizeof(b));
 return (0);
 }
