fc6e9aff864e6dbeceb9a91cfafb31b620c2d9e1
Constance Amatey <consamate57@gmail.com>
2023-02-16 19:01:52 -0800

    c1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - My second c programme
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper 6, but the outcome is a piece of art,");
+	return (0);
+}
