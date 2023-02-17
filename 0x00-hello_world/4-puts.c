fcff2f55f998277057408977fbc7952f71a492e8
Constance Amatey <consamate57@gmail.com>
2023-02-16 18:51:11 -0800

    c1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My first c programme
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
