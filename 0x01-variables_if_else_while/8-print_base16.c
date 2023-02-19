db79e0ca515c71db4c4ec1efa24bd42e9cea2e60
Constance Amatey <consamate57@gmail.com>
2023-02-18 21:18:29 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	char letters;
+
+	for (number = 0; number < 10; number++)
+		putchar('0' + number);
+	for (letters = 'a'; letters <= 'f'; letters++)
+		putchar(letters);
+	putchar('\n');
+	return (0);
+}
