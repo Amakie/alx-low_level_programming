a964c88e676d662e3a7f987a5a2c84f56c468aa4
Constance Amatey <consamate57@gmail.com>
2023-02-18 18:29:53 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lower_upper:
+
+	for (lower_upper = 'a'; lower_upper <= 'z'; lower_upper++)
+		putchar(lower_upper);
+	for (lower_upper = 'A'; lower_upper <= 'Z'; lower_upper++)
+		putchar(lower_upper);
+	putchar('\n');
+	return (0);
+}
