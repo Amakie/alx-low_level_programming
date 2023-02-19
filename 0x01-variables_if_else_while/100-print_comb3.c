7141824a57a2760cc899b4ee4d333a2c4f077b14
Constance Amatey <consamate57@gmail.com>
2023-02-18 22:01:02 -0800

    print combination 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - first line
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first < 9; first++)
+	{
+		for (second = first + 1; second < 10; second++)
+		{
+			putchar('0' + first);
+			putchar('0' + second);
+
+			if (first == 8 && second == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
