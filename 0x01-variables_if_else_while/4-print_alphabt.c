3f08d8f60f183782a86ef3d31bb6b773a92524ac
Constance Amatey <consamate57@gmail.com>
2023-02-18 20:18:47 -0800

    except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,19 @@
-#include <
+#include <stdio.h>
+/**
+ * main - alphabets except
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char qNe;
+
+	for (qNe = 'a'; qNe <= 'Z'; qNe++)
+	{
+		if (qNe != 'e' && qNe != 'q')
+			putchar(qNe);
+	}
+	putchar('\n');
+	return (0);
+}
