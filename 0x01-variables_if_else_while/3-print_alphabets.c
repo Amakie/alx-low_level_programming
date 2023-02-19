dc6b61f1d7d4ae7eddd8c9280e780e234d634d00
Constance Amatey <consamate57@gmail.com>
2023-02-18 19:03:49 -0800

    print upper and lower cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - print alphabets
+ * main - Entry point
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	char lower_upper:
+	char lower_upper;
 
 	for (lower_upper = 'a'; lower_upper <= 'z'; lower_upper++)
 		putchar(lower_upper);
