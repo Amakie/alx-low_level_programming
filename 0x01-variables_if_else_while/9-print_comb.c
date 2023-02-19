f7183e45762cb583fca21d5c4903d4f201dc6403
Constance Amatey <consamate57@gmail.com>
2023-02-18 21:43:20 -0800

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print letters and numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb < 10; comb++)
+	{
+		putchar('0' + comb);
+		if (comb == 9)
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
