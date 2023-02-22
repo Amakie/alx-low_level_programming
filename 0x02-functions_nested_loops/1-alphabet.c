c3ca6a3940d8c69490dfc11dc6f9e8a2fc5e2a67
Constance Amatey <consamate57@gmail.com>
2023-02-21 17:50:01 -0800

    print lower cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Prints alphabets to screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char b;
+
+	for (b = "a"; b <= "z"; b++)
+		_putchar("%s", b);
+	_putchar("\n");
+	Return(0);
+}
