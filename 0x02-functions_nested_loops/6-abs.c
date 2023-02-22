7d6ae4f2d928c22d02ae0693b1f603e747ede8c7
Constance Amatey <consamate57@gmail.com>
2023-02-22 12:43:51 -0800

    print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value
+ * @i: integer to be computed
+ *
+ * Return: Absolute value.
+ */
+int _abs(int i);
+{
+	if (x >= 0)
+		return (i);
+	else
+		return (-i);
+}
