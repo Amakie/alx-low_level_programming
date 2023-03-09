c1e2261e4d2c19481e9d379b2a0b92bb015b8d8a
Constance Amatey <consamate57@gmail.com>
2023-03-08 18:25:55 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * factorial - calculates a factorial of a number
+ * @n: input variable
+ * Return: Always 0
+ */
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		_putchar(n * (n - 1));
+	}
+	else
+	{
+		return (-1);
+	}
+}
