27738019f710185a19511abc7df24edf5eb9d074
Constance Amatey <consamate57@gmail.com>
2023-03-09 16:29:12 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,15 +4,14 @@
  * @x: input variable
  * @y: second input variable
  * Return: Always 0
+ *
  */
 int _pow_recursion(int x, int y)
 {
-	if (y >= 0);
-	{
-		return (_pow_recursion (x ^ y));
-	}
-	if y < 0;
-	{
+	if (y < 0)
 		return (-1);
-	}
+	if (y == 0)
+		return (1);
+
+	return (x * (_pow_recursion(x, y - 1)));
 }
