8855b25cb61eb542b2e7fb63ebaf6d7db6afc737
Constance Amatey <consamate57@gmail.com>
2023-03-13 22:52:23 -0700

    multiply 2 numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - program that multiplies two numbers
+ * @number_1: first number to be multiplied
+ * @number_2: second number to be multiplied
+ * @product: the result of the two numbers
+ * @argc: counts the number of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, int number_1, int number_2, int product)
+{
+	product = number_1 * number_2;
+
+	if (argc == 2)
+
+	{
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("error\n");
+
+		return (1);
+	}
+	return (0);
+}
