1a3bfed7af262525ff78fab23aca662a5c001f7e
Constance Amatey <consamate57@gmail.com>
2023-03-14 13:11:18 -0700

    returns a pptr

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array
+ * @c: character to the pointer
+ * @size: size of the unsigned integer
+ *
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr[];
+	unsigned int size = n;
+
+	if (size == 0)
+	{
+		malloc(n * sizeof(char c));
+		return (NULL);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
