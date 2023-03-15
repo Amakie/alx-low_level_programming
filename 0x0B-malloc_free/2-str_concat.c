0a1f2ed25acf4998cca2035e0abd359b47ac0cd0
Constance Amatey <consamate57@gmail.com>
2023-03-14 18:02:29 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenate two strings using malloc
+ * str_concat - concatenate two strings
  * @s1: string 1
  * @s2: string 2
  * Return: pointer to concat string
@@ -11,28 +11,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	int e, f, g, d;
+	int i, j, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (e = 0; s1[e] != '\0'; e++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (f = 0; s2[f] != '\0'; f++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	a = malloc((e * sizeof(*s1)) + (f * sizeof(*s2)) + 1);
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
 	if (a == NULL)
 		return (NULL);
 
-	for (g = 0, d = 0; c < (e + f + 1); g++)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		if (g < e)
-			a[g] = s1[g];
+		if (c < i)
+			a[c] = s1[c];
 		else
-			a[g] = s2[d++];
+			a[c] = s2[d++];
 	}
 
 	return (a);
