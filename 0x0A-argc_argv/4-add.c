b6bb2cb95f57360dbe5c1a11752c2534f91538f9
Constance Amatey <consamate57@gmail.com>
2023-03-14 20:03:53 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: counts number of argument
+ * @argv: arrays to the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	sum = num1 + num2
+	{
+		printf("%d\n", sum);
+	}
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	if (argv ![num1, num2])
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
