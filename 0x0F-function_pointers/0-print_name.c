7244bf2270f7adc41c8ba95a00a1907e45029d74
Constance Amatey <consamate57@gmail.com>
2023-03-22 13:55:26 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - print name of function
+ * @name: pointer of name
+ * @f: pointer of function
+ *
+ * Return: Always zero
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	(*f)(name);
+}
+{
+	_putchar('f');
+	_putchar('\n');
+}
