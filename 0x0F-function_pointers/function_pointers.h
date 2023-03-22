a8d0e423ef2ef2d683851535e3eb7bbde46d35fa
Constance Amatey <consamate57@gmail.com>
2023-03-22 13:18:06 -0700

    files

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif 
