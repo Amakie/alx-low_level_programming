6fed1645fd63ea1171cd3cd59664091b17da1558
Constance Amatey <consamate57@gmail.com>
2023-03-22 17:02:52 -0700

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int c;
-	
 	if (array == NULL || action == NULL)
 		return;
+
+	int c;
 
 	for (c = 0; c < size; c++)
 		action(array[c]);
