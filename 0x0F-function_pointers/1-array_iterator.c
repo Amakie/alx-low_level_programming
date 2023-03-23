89c0e6d4c74dacd6b9dc4b5252ae8df7e9eedfe6
Constance Amatey <consamate57@gmail.com>
2023-03-22 17:36:33 -0700

    iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,11 +10,14 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int c;
+	unsigned int c = 0;
 
-	if (array == NULL || action == NULL)
+	if (array == NULL || action == NULL || size > 0)
 	{
-		for (c = 0; c < size; c++)
+		while (c < size)
+		{
 			action(array[c]);
+			c++;
+		}
 	}
 }
