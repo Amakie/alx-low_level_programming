e812682bef4fa1dfe1b6918635d958f50ccbe26b
Constance Amatey <consamate57@gmail.com>
2023-03-23 17:42:08 -0700

    sun

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,21 +9,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list myintegers;
+
 	int a;
 
 	int sum;
+
+	va_start(myintegers, n)
 
 	if (n == 0)
 		return (0);
 
 	for (a = 0; a < n; a++)
-	{
-	va_list myintegers;
-
-	va_start(myintegers, n);
 
 	sum = n + va_arg(myintegers);
+
 	va_end(myintegers);
-	}
+
 	return (sum);
 }
