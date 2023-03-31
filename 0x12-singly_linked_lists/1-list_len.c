877295151963b1186dbbad78b26886a7717b26a4
Constance Amatey <consamate57@gmail.com>
2023-03-30 18:25:20 -0700

    print nodes number

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
-
 /**
- * list_len - prints the number of elements
- * @h: singly linked list
- *
- * Return: number of elements
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
+
 size_t list_len(const list_t *h)
 {
-	size_t number_of_elements;
+	size_t number_of_nodes;
 
-	number_of_elements = 0;
-
-	while (h != 0)
+	number_of_nodes = 0;
+	while (h != NULL)
+	{
 		h = h->next;
-		number_of_elements++;
-
-	return (number_of_elements);
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
 }
