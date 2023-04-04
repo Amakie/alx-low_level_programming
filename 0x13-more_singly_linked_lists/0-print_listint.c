5fb5ecc2b025dd30cf76d17054cbf48f019ae24d
Constance Amatey <consamate57@gmail.com>
2023-04-04 09:42:49 -0700

    print the nodes and return the number of nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the members of the list
+ * @h: pointer to the first node
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+			number++;
+	}
+	return (number);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+tasks on more singly lists. The singly lists allows you to add more inputs to a data set with more flexibility
