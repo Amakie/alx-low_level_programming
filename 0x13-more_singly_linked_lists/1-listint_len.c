a16c8f977a381d1382af43e3870853f2f8605b96
Constance Amatey <consamate57@gmail.com>
2023-04-04 10:00:02 -0700

    number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the number of nodes in the list
+ * @h: pointer to the head
+ *
+ * Return: Number of node
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		number++;
+	}
+	return (number);
+}
