a10902904075cb36544b714cd7100df8ebb03365
Constance Amatey <consamate57@gmail.com>
2023-04-04 20:08:09 -0700

    frees list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function that frees a list
+ * @head: pointer to the list
+ *
+ * Return: 0
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	if (*head == NULL)
+		return;
+
+	while (*head)
+	{
+		new = *head;
+		*head = (*head)->next;
+		free(new);
+	}
+	head = NULL;
+}
