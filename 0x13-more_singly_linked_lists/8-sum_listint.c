fd2458813d3044355a27fdfaf9b42df3fc6fa9b2
Constance Amatey <consamate57@gmail.com>
2023-04-05 11:47:16 -0700

    adds data in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - adds the sum of all data n
+ * @head: pointer that points to the list
+ * @n: interget
+ *
+ * Return: 0
+ */
+int sum_listint(listint_t *head)
+{
+	int add = 0;
+
+	add = add + head->n;
+
+	if (head == NULL)
+		return (0);
+	else
+		return (add);
+}
