c977942e8dac14d13db00963385afb2a0cd8d608
Constance Amatey <consamate57@gmail.com>
2023-04-05 10:52:49 -0700

    iterates to the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,6 +2,7 @@
 /**
  * get_nodeint_at_index - returns the nth node of the list
  * @head: pointer to the list
+ * @index: index of the node to be found
  *
  * Return: 0
  */
@@ -9,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t n;
 
-	for (n = 1; (n < index) && (head->next); n++)
+	for (n = 0; (n < index) || (head->next); n++)
 		head = head->next;
 	if (n < index)
 		return (NULL);
