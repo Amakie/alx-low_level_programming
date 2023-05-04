1b45fcc68cd892d37b11a4c401fc0114a6cbf377
root <consamate57@gmail.com>
2023-05-04 07:30:55 -0700

    convert binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: pointer
+ *
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int conv_num = 0;
+	int a = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (a = 0; b[a] == '0' || b[a] == '1'; a++)
+	{
+		conv_num <<= 1;
+		conv_num += b[a] - '0';
+	}
+
+	return (conv_num);
+}
