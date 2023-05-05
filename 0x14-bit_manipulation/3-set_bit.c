60803ffecf81bb0c4d88198b31ecdd5983b0ce96
root <consamate57@gmail.com>
2023-05-04 19:05:13 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,12 +10,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int set_ind;
-
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	set_ind = ~(1UL << index);
-	*n = *n & set_ind;
+	*n |= (1UL << index);
 	return (1);
 }
