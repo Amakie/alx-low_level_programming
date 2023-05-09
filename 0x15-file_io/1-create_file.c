2360c9aaacedc839d4243482065213f4131579d3
root <consamate57@gmail.com>
2023-05-09 09:44:39 -0700

    create function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,10 +38,10 @@ int create_file(const char *filename, char *text_content)
 
 	perm = write(file_des, text_content, num_lett);
 	if (perm == -1)
-
+	{
 		close(file_des);
 		return (-1);
-
+	}
 
 	close(file_des);
 	return (1);
