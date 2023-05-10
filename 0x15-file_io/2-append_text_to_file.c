84dd82bd27fd35624ce7fa3a639715decd6adc48
root <consamate57@gmail.com>
2023-05-10 05:00:36 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file_des = open(filename, O_APPEND);
+	file_des = open(filename, O_WRONLY, O_APPEND);
 
 	if (file_des == -1)
 	{
