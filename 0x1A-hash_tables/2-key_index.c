29615a8377d91549aabb1ec84ae6cab6b6996c25
Constance Amatey <consamate57@gmail.com>
2023-09-08 08:05:12 -0700

    gives index of key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_table.h"
+
+/**
+ * key_index - gets index for where key of hash table is stored
+ * @size: size of hash table array
+ * @key: Key for the index
+ *
+ * Return: index of key using the djb2 algorithm
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
